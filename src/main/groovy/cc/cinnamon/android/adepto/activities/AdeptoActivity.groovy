6e7042048034fff441303c18133cb4a0f9448007
carddamom <carddamom@outlook.pt>
2016-03-28 15:27:30 +0100

    Added the initial files.

---
+++ src/main/groovy/cc/cinnamon/android/adepto/activities/AdeptoActivity.groovy
@@ -0,0 +1,10 @@
+package cc.cinnamon.android.adepto.activities
+
+import android.app.Activity
+import android.os.Bundle
+
+class AdeptoActivity extends Activity {
+    public onCreate(Bundle savedInstanceState) {
+        super.onCreate savedInstanceState
+    }
+}
